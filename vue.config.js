e75f6ea10dfd9bdfc6a3c704b20af7d4fba12a1d
sytv <transy97@gmail.com>
2022-09-13 10:46:14 +0700

    update 13.9.1

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
+/* eslint-disable */
+
 const path = require('path')
 
-export const pluginOptions = {
-  'style-resources-loader': {
-    preProcessor: 'scss',
-    patterns: [
-      resolve(__dirname, 'src/styles/_variables.scss'),
-      resolve(__dirname, 'src/styles/_mixins.scss')
-    ]
-  }
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/styles/_variables.scss'),
+        path.resolve(__dirname, 'src/styles/_mixins.scss')
+      ]
+    }
+  },
 }
